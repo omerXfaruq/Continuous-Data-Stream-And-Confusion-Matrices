bf0c46951ae7153caa09e4c9ae9aedcadc2d64bb
Ömer Faruk Özdemir <farukozderim@gmail.com>
2021-12-10 20:53:22 +0300

    Data-Source Completed - Refactor the code. - Create concurrent Continous Learning structure. - Create runnable main program.

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from .main import ContinuousLearning
 
 if __name__ == "__main__":
     if len(argv) != 3:
-        exit("Please provide an input file")
+        exit("Please provide an input file and close_after_seconds duration!")
     input_path = argv[1]
     close_after_seconds = int(argv[2])
     ContinuousLearning(input_path=input_path, close_after_time=close_after_seconds)
